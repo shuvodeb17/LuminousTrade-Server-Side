bed237868709a9c56e2cfd7df9366b43d7d40182
Shuvo Deb <shuvodeb17014@gmail.com>
2023-10-29 19:54:43 +0600

    product api create

--- src/controllers/multerImageUploads.js
+++ src/controllers/multerImageUploads.js
@@ -3,11 +3,15 @@ const addProducts = require("../models/multerImageModel");
 const imagePost = async (req, res) => {
   try {
     const file = req.file;
-    const { name } = req.body;
+    const { name, email, price, title, details } = req.body;
 
     const newProduct = new addProducts({
-      name: name,
       image: file.filename,
+      name: name,
+      email: email,
+      price: price,
+      title: title,
+      details: details,
     });
     await newProduct.save();
     res.status(200).json({
@@ -18,5 +22,7 @@ const imagePost = async (req, res) => {
     console.log(error);
   }
 };
+
+
 
 module.exports = { imagePost };
--- src/models/multerImageModel.js
+++ src/models/multerImageModel.js
@@ -1,10 +1,22 @@
 const { Schema, model } = require("mongoose");
 
 const addProductSchema = new Schema({
+  image: {
+    type: String,
+  },
   name: {
     type: String,
   },
-  image: {
+  email: {
+    type: String,
+  },
+  price: {
+    type: Number,
+  },
+  title: {
+    type: String,
+  },
+  details: {
     type: String,
   },
 });
